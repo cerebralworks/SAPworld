71c9e3bf63255f00a312a56870dfd696a4660127
ArunKumar S <arunkumar.s@agnitio-systems.com>
2021-08-03 14:23:47 +0530

    Job Matches Logic Change

--- API/api/models/ScoreMaster.js
+++ API/api/models/ScoreMaster.js
@@ -10,25 +10,8 @@ module.exports = {
     tableName: 'score_master',
     tableAlias: 'score_masters',
     attributes: {
-        total_experience: { type: 'number' , defaultsTo: 1},
-        sap_experience: { type: 'number' , defaultsTo: 1},
-        hands_on_experience: { type: 'number', defaultsTo: 1 },
-        job_types: { type: 'number', defaultsTo: 1 },
-        work_auth: { type: 'number' , defaultsTo: 1},
-        job_location: { type: 'number', defaultsTo: 1 },
-        knowledge: { type: 'number', defaultsTo: 1 },
-        end_to_end_implemention: { type: 'number' , defaultsTo: 0.5},
-        education: { type: 'number' , defaultsTo: 0.25},
-        job_role: { type: 'number' , defaultsTo: 1},
-        availability: { type: 'number' , defaultsTo: 1},
-        programming: { type: 'number' , defaultsTo: 1},
-        other_skills: { type: 'number', defaultsTo: 1 },
-        certification: { type: 'number' , defaultsTo: 1},
-        domain: { type: 'number' , defaultsTo: 1},
-        remote: { type: 'number' , defaultsTo: 0.5},
-        travel: { type: 'number' , defaultsTo: 0.5},
-        language: { type: 'number' , defaultsTo: 0.25},
-        total: { type: 'number' , defaultsTo: 150}
-		
+        required: { type: 'number' , defaultsTo: 1},
+        desired: { type: 'number' , defaultsTo: 0.5},
+        optional: { type: 'number', defaultsTo: 0.25 },		
     }
 };
