836eed73f71b74f6f252b25aa72f5f1e50df4cba
ArunKumar S <arunkumar.s@agnitio-systems.com>
2021-10-30 18:22:13 +0530

    Bug Fixing

--- API/api/controllers/invitestatus/create.js
+++ API/api/controllers/invitestatus/create.js
@@ -8,6 +8,7 @@ module.exports = async function create(request, response) {
 	if(dataCheck && dataCheck.length && dataCheck.length !=0){
 		request_data['job_applications'] = dataCheck[0]['answer'];
 	}
+	
 	if(request_data['job_applications']){
 		JobApplications.findOne({where :{id : request_data['job_applications']}}).then(data=>{
 			if(data){
@@ -19,6 +20,7 @@ module.exports = async function create(request, response) {
 			}
 		})
 	}
+	console.log(request_data);
         InviteStatus.create(request_data).then(function(data) {
            _response_object.details = data;
            return response.status(201).json(_response_object);
