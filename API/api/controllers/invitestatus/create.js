66c48c2874a6a142173d1721a35064083cff2e2f
ArunKumar S <arunkumar.s@agnitio-systems.com>
2021-11-01 18:12:23 +0530

    Bug Fixing

--- API/api/controllers/invitestatus/create.js
+++ API/api/controllers/invitestatus/create.js
@@ -8,7 +8,9 @@ module.exports = async function create(request, response) {
 	if(dataCheck && dataCheck.length && dataCheck.length !=0){
 		request_data['job_applications'] = dataCheck[0]['answer'];
 	}
-	
+	if(request_data['name']=='invitee.canceled'){
+		request_data['canceled'] = true;
+	}
 	
 	console.log(request_data);
         InviteStatus.create(request_data).then(function(data) {
@@ -16,10 +18,10 @@ module.exports = async function create(request, response) {
 			  // console.log(data);
 				JobApplications.findOne({where :{id : data['job_applications']}}).then(datas=>{
 					if(datas){
-						console.log(data);
+						//console.log(data);
 						//console.log(datas['invite_status']);
 						var _response_objects = {'invite_status': false,'reschedule_url': data['reschedule_url'],'cancel_url': data['cancel_url'],'canceled': data['canceled'],'rescheduled': data['rescheduled']};
-						console.log(_response_objects);
+						//console.log(_response_objects);
 						JobApplications.update(datas.id,_response_objects).then(da=>{
 							// console.log(da);
 							return response.status(200).json(da);
