daeadf40f2958a97d6181c6255854adb5e910994
ArunKumar S <arunkumar.s@agnitio-systems.com>
2021-11-01 16:53:14 +0530

    Bug Fixing

--- API/api/controllers/invitestatus/create.js
+++ API/api/controllers/invitestatus/create.js
@@ -3,22 +3,22 @@ module.exports = async function create(request, response) {
 
     var _response_object = {};
     var request_data = request.body.payload;
-	console.log(request_data);
 	request_data['name'] = request.body.event;
 	var dataCheck = request.body.payload['questions_and_answers'];
 	if(dataCheck && dataCheck.length && dataCheck.length !=0){
 		request_data['job_applications'] = dataCheck[0]['answer'];
 	}
-
-	console.log(request_data);
+	
+	
+	//console.log(request_data);
         InviteStatus.create(request_data).then(function(data) {
            if(data['job_applications']){
-			   console.log(data);
+			  // console.log(data);
 				JobApplications.findOne({where :{id : data['job_applications']}}).then(datas=>{
 					if(datas){
 						console.log(datas);
 						console.log(datas['invite_status']);
-						var _response_objects = {'invite_status': false};
+						var _response_objects = {'invite_status': false,'reschedule_url': datas['reschedule_url'],'cancel_url': datas['cancel_url'],'canceled': datas['canceled'],'rescheduled': datas['rescheduled']};
 						//datas['invite_status'] = false;
 						JobApplications.update(datas.id,_response_objects).then(da=>{
 							 console.log(da);
