9ea212f0dfdf195114d57d55ef4e8a8484104023
ArunKumar S <arunkumar.s@agnitio-systems.com>
2021-11-02 15:18:16 +0530

    Reschedule bug fixing

--- API/api/controllers/invitestatus/create.js
+++ API/api/controllers/invitestatus/create.js
@@ -25,14 +25,13 @@ module.exports = async function create(request, response) {
 							else {
 								datas['application_status'][datas['application_status'].length-1]['created']= new Date();
 							}
-							if(data['rescheduled'] == true ){
+							if(data['rescheduled'] == true || data['rescheduled'] == 'true' ){
 								datas['application_status'][datas['application_status'].length-1]['rescheduled']= new Date();
 							}
 							
 						}
 						var _response_objects = {'invite_status': false,'application_status': datas['application_status'],'reschedule_url': data['reschedule_url'],'cancel_url': data['cancel_url'],'canceled': data['canceled'],'rescheduled': data['rescheduled']};
-						console.log(_response_objects);
-						console.log(data);
+						
 						JobApplications.update(datas.id,_response_objects).then(da=>{
 							
 							return response.status(200).json(da);
