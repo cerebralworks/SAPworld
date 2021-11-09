6e0c25b11f09a46d01364e1624e0efa14c5d900b
ArunKumar <arunkumar.s@agnitio-systems.com>
2021-11-09 14:40:51 +0530

    Fixing the application status update

--- API/api/controllers/jobpostings/applications/short-list-user.js
+++ API/api/controllers/jobpostings/applications/short-list-user.js
@@ -135,6 +135,30 @@ module.exports = async function update(request, response) {
 				await UserProfiles.findOne(filtered_post_data.user, async function(err, profile) {
 					filtered_post_data.employer = _.get(logged_in_user, 'employer_profile.id');
 					if (job_application) {
+						var checkLastItem = filtered_post_data.application_status[filtered_post_data.application_status.length-1];
+						var checkLastItemServer = job_application.application_status[job_application.application_status.length-1];
+						
+						if(checkLastItem.id === checkLastItemServer.id){
+
+						}else{
+							var sliceCheck =job_application.application_status;
+							var filteredData =sliceCheck.map((val) => {
+							  return { 
+								id: val.id,
+								status: val.status,
+								date: val.date,
+								comments: val.comments,
+								invited: val.invited,
+								canceled: val.canceled,
+								rescheduled: val.rescheduled,
+								created: val.created,
+								invite_url: ''
+							  }
+							});
+							filteredData.push(checkLastItem);
+							filtered_post_data.application_status = filteredData;
+						}
+						
 						updateJobApplication(filtered_post_data, async function(job_application) {
 							sendResponse(job_application,job,profile,logged_in_user);
 						});
