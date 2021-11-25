af61f603ce8168932a70704477354389fe64c47d
ArunKumar <arunkumar.s@agnitio-systems.com>
2021-11-25 16:58:58 +0530

    Bug Fixing

--- API/api/controllers/invitestatus/create.js
+++ API/api/controllers/invitestatus/create.js
@@ -48,7 +48,7 @@ const requests = require('request');
 							var arrayVal = request_data['events'];
 							datas['events']=[arrayVal]
 						}
-						
+						console.log(datas['events']);
 						var _response_objects = {'events': datas['events'],'invite_status': false,'application_status': datas['application_status'],'reschedule_url': data['reschedule_url'],'cancel_url': data['cancel_url'],'canceled': data['canceled'],'rescheduled': data['rescheduled']};
 						console.log(_response_objects);
 						JobApplications.update(datas.id,_response_objects).then(da=>{
