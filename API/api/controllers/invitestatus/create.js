ebb026c178bfa4effc1ca55378744d2868a007a3
ArunKumar <arunkumar.s@agnitio-systems.com>
2021-11-25 16:56:06 +0530

    Bug Fixing

--- API/api/controllers/invitestatus/create.js
+++ API/api/controllers/invitestatus/create.js
@@ -19,12 +19,12 @@ const requests = require('request');
 		Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjM3MzE5MTcxLCJqdGkiOiIxNjk5MGFiZi1kYzQxLTQ0MGQtOTJmNy1iZjcyOWE4ZjZmOWMiLCJ1c2VyX3V1aWQiOiIyMGMzYjIyMi03NzNkLTQ5MzctOGQwYi05MmM0YWI0MDViYWYifQ.tR-IOMTAoniqaJuTiHWdS2chcFSoch44YpF5xj9qobc'
 	  }
 	};
-	requests(options,async function (error, response, body) {
+	requests(options,async function (error, responses, body) {
 	  if (error) throw new Error(error);
 
 	  if(body){
 		  request_data['events']={'data':body};
-		  console.log(request_data['events']);
+		  //console.log(request_data['events']);
         InviteStatus.create(request_data).then(async function(data) {
            if(data['job_applications']){
 			  
@@ -48,12 +48,12 @@ const requests = require('request');
 							var arrayVal = request_data['events'];
 							datas['events']=[arrayVal]
 						}
-						console.log(request_data['events']);
-						var _response_objects = {'events': datas['events'],'invite_status': false,'application_status': datas['application_status'],'reschedule_url': data['reschedule_url'],'cancel_url': data['cancel_url'],'canceled': data['canceled'],'rescheduled': data['rescheduled']};
 						
+						var _response_objects = {'events': datas['events'],'invite_status': false,'application_status': datas['application_status'],'reschedule_url': data['reschedule_url'],'cancel_url': data['cancel_url'],'canceled': data['canceled'],'rescheduled': data['rescheduled']};
+						console.log(_response_objects);
 						JobApplications.update(datas.id,_response_objects).then(da=>{
 							//console.log(da);
-							//return response.status(200).json(da);
+							return response.status(200).json(da);
 						});
 
 					}
@@ -65,7 +65,7 @@ const requests = require('request');
                await errorBuilder.build(err, function(error_obj) {
                    _response_object.errors = error_obj;
                    _response_object.count = error_obj.length;
-                   //return response.status(500).json(_response_object);
+                   return response.status(500).json(_response_object);
                });
                });
 	  }
