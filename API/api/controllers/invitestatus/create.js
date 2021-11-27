b9fbfc883b202771abdfef84bc02680e5ada47dd
ArunKumar <arunkumar.s@agnitio-systems.com>
2021-11-27 14:58:25 +0530

    Bug Fixing

--- API/api/controllers/invitestatus/create.js
+++ API/api/controllers/invitestatus/create.js
@@ -11,104 +11,103 @@ const requests = require('request');
 	if(request_data['name']=='invitee.canceled'){
 		request_data['canceled'] = true;
 	}
-	
-	
-    InviteStatus.create(request_data).then(async function(data) {
-		const options = {
-		  method: 'GET',
-		  url: data['event'],
-		  headers: {
-			'Content-Type': 'application/json',
-			Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjM3MzE5MTcxLCJqdGkiOiIxNjk5MGFiZi1kYzQxLTQ0MGQtOTJmNy1iZjcyOWE4ZjZmOWMiLCJ1c2VyX3V1aWQiOiIyMGMzYjIyMi03NzNkLTQ5MzctOGQwYi05MmM0YWI0MDViYWYifQ.tR-IOMTAoniqaJuTiHWdS2chcFSoch44YpF5xj9qobc'
-		  }
-		};
-	
-		requests(options,async function (error, responses, body) {
-			if (error) throw new Error(error);
-		
-			if(body){
-				request_data['events']={'data':body};
-				
-				if(data['job_applications']){
-				  
-					JobApplications.findOne({where :{id : data['job_applications']}}).then(datas=>{
-						if(datas){
-							if(datas['application_status'].length && datas['application_status'].length !=0){
-								if(data['canceled'] == true && (data['rescheduled'] != true && data['rescheduled'] != 'true') ){
-									datas['application_status'][datas['application_status'].length-1]['canceled']= new Date();
+	const options = {
+	  method: 'GET',
+	  url: request_data['event'],
+	  headers: {
+		'Content-Type': 'application/json',
+		Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjM3MzE5MTcxLCJqdGkiOiIxNjk5MGFiZi1kYzQxLTQ0MGQtOTJmNy1iZjcyOWE4ZjZmOWMiLCJ1c2VyX3V1aWQiOiIyMGMzYjIyMi03NzNkLTQ5MzctOGQwYi05MmM0YWI0MDViYWYifQ.tR-IOMTAoniqaJuTiHWdS2chcFSoch44YpF5xj9qobc'
+	  }
+	};
+	console.log(request_data['event']);
+	requests(options,async function (error, responses, body) {
+	  if (error) throw new Error(error);
+
+	  if(body){
+		  
+		  request_data['events']={'data':body};
+        InviteStatus.create(request_data).then(async function(data) {
+           if(data['job_applications']){
+			  
+				JobApplications.findOne({where :{id : data['job_applications']}}).then(datas=>{
+					if(datas){
+						if(datas['application_status'].length && datas['application_status'].length !=0){
+							if(data['canceled'] == true && (data['rescheduled'] != true && data['rescheduled'] != 'true') ){
+								datas['application_status'][datas['application_status'].length-1]['canceled']= new Date();
+								
+								if(datas['events'] && datas['events'].length && datas['events'].length !=0){
+									var arrayVal = request_data['events'];
+									arrayVal['status']=datas['status'];
+									arrayVal['canceled']= new Date();
+									datas['events'].push(arrayVal);
 									
-									if(datas['events'] && datas['events'].length && datas['events'].length !=0){
-										var arrayVal = request_data['events'];
-										arrayVal['status']=datas['status'];
-										arrayVal['canceled']= new Date();
-										datas['events'].push(arrayVal);
-										
-									}else{
-										var arrayVal = [request_data['events']];
-										arrayVal['status']=datas['status'];
-										arrayVal['canceled']= new Date();									
-										datas['events']=[arrayVal]
-									}
-							
-								}else if(data['rescheduled'] != true && data['rescheduled'] != 'true' && data['canceled'] == false  && datas['rescheduled'] != true && datas['rescheduled'] != 'true'){
-									datas['application_status'][datas['application_status'].length-1]['created']= new Date();
+								}else{
+									var arrayVal = data['events'];
+									arrayVal['status']=datas['status'];
+									arrayVal['canceled']= new Date();									
+									datas['events']=[arrayVal]
+								}
+						
+							}else if(data['rescheduled'] != true && data['rescheduled'] != 'true' && data['canceled'] == false  && datas['rescheduled'] != true && datas['rescheduled'] != 'true'){
+								datas['application_status'][datas['application_status'].length-1]['created']= new Date();
+								
+								if(datas['events'] && datas['events'].length && datas['events'].length !=0){
+									var arrayVal = request_data['events'];
+									arrayVal['status']=datas['status'];
+									arrayVal['created']= new Date();
+									datas['events'].push(arrayVal);
 									
-									if(datas['events'] && datas['events'].length && datas['events'].length !=0){
-										var arrayVal = request_data['events'];
-										arrayVal['status']=datas['status'];
-										arrayVal['created']= new Date();
-										datas['events'].push(arrayVal);
-										
-									}else{
-										var arrayVal = [request_data['events']];
-										arrayVal['status']=datas['status'];
-										arrayVal['created']= new Date();
-										datas['events']=[arrayVal]
-									}
+								}else{
+									var arrayVal = data['events'];
+									arrayVal['status']=datas['status'];
+									arrayVal['created']= new Date();
+									datas['events']=[arrayVal]
+								}
+								
+							}else if(data['rescheduled'] == true || data['rescheduled'] == 'true' ){
+								datas['application_status'][datas['application_status'].length-1]['rescheduled']= new Date();
+								
+								
+								if(datas['events'] && datas['events'].length && datas['events'].length !=0){
+									var arrayVal = request_data['events'];
+									arrayVal['status']=datas['status'];
+									arrayVal['rescheduled']= new Date();
+									datas['events'].push(arrayVal);
 									
-								}else if(data['rescheduled'] == true || data['rescheduled'] == 'true' ){
-									datas['application_status'][datas['application_status'].length-1]['rescheduled']= new Date();
-									
-									
-									if(datas['events'] && datas['events'].length && datas['events'].length !=0){
-										var arrayVal = request_data['events'];
-										arrayVal['status']=datas['status'];
-										arrayVal['rescheduled']= new Date();
-										datas['events'].push(arrayVal);
-										
-									}else{
-										var arrayVal = [request_data['events']];
-										arrayVal['status']=datas['status'];
-										arrayVal['rescheduled']= new Date();
-										datas['events']=[arrayVal]
-									}
-									
+								}else{
+									var arrayVal = data['events'];
+									arrayVal['status']=datas['status'];
+									arrayVal['rescheduled']= new Date();
+									datas['events']=[arrayVal]
 								}
 								
 							}
 							
-							
-							var _response_objects = {'events': datas['events'],'invite_status': false,'application_status': datas['application_status'],'reschedule_url': data['reschedule_url'],'cancel_url': data['cancel_url'],'canceled': data['canceled'],'rescheduled': data['rescheduled']};
-							console.log('1');
-							console.log(_response_objects);
-							JobApplications.update(datas.id,_response_objects).then(da=>{
-								return response.status(200).json(da);
-							});
-
 						}
 						
-					})
-				}
+						
+						var _response_objects = {'events': datas['events'],'invite_status': false,'application_status': datas['application_status'],'reschedule_url': data['reschedule_url'],'cancel_url': data['cancel_url'],'canceled': data['canceled'],'rescheduled': data['rescheduled']};
+						console.log('1');
+						console.log(_response_objects);
+						JobApplications.update(datas.id,_response_objects).then(da=>{
+							return response.status(200).json(da);
+						});
+
+					}
+					
+				})
 			}
-		});
-		
-	}).catch(async function(err) {
-	   await errorBuilder.build(err, function(error_obj) {
-		   _response_object.errors = error_obj;
-		   _response_object.count = error_obj.length;
-		   return response.status(500).json(_response_object);
-	   });
+           
+           }).catch(async function(err) {
+               await errorBuilder.build(err, function(error_obj) {
+                   _response_object.errors = error_obj;
+                   _response_object.count = error_obj.length;
+                   return response.status(500).json(_response_object);
+               });
+               });
+	  }
+	  
 	});
 	
-	
-}    
+       }
+       
