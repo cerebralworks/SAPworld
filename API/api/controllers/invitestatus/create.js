9c45739530e51e06b528c71a448491db730f72d2
ArunKumar <arunkumar.s@agnitio-systems.com>
2021-11-30 14:44:02 +0530

    Adding Notification in scheduled events

--- API/api/controllers/invitestatus/create.js
+++ API/api/controllers/invitestatus/create.js
@@ -1,5 +1,6 @@
 /* global _, invite_status, sails */
 module.exports = async function create(request, response) {
+	
 	const requests = require('request');
     var _response_object = {};
     var request_data = request.body.payload;
@@ -20,30 +21,42 @@ module.exports = async function create(request, response) {
 		Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjM3MzE5MTcxLCJqdGkiOiIxNjk5MGFiZi1kYzQxLTQ0MGQtOTJmNy1iZjcyOWE4ZjZmOWMiLCJ1c2VyX3V1aWQiOiIyMGMzYjIyMi03NzNkLTQ5MzctOGQwYi05MmM0YWI0MDViYWYifQ.tR-IOMTAoniqaJuTiHWdS2chcFSoch44YpF5xj9qobc'
 	  }
 	};
-	console.log(request.body)
-	console.log(request_data)
-	console.log(request_data['event']);
 	
+	/**
+	**	To get the scheduled events
+	**/
 	requests(options,async function (error, responses, body) {
+		
 		if (error) throw new Error(error);
 
 		if(body){
 		  
 			request_data['events']={'data':body};
+			
+			/**
+			**	To create a InviteStatus 
+			**/
 			InviteStatus.create(request_data).then(async function(data) {
 				
 				if(data['job_applications']){
-			  
+					
+					/**
+					**	To get the job application
+					**/
 					JobApplications.findOne({where :{id : data['job_applications']}}).then(datas=>{
-						
-						console.log(request_data['event']);
 						
 						if(datas){
 							
+							var postDetailss = {};
+							var titleName = datas['application_status'][datas['application_status']['length']-1]['status'];
+							
+							/**
+							**	To validate the application events and push the values
+							**/
 							if(datas['application_status'].length && datas['application_status'].length !=0){
 								
 								if(data['canceled'] == true && (data['rescheduled'] != true && data['rescheduled'] != 'true') ){
-									
+									postDetailss.message= postDetails.name+' is Canceled ';
 									datas['application_status'][datas['application_status'].length-1]['canceled']= new Date();
 
 									if(datas['events'] && datas['events'].length && datas['events'].length !=0){
@@ -62,7 +75,7 @@ module.exports = async function create(request, response) {
 									}
 							
 								}else if(data['rescheduled'] != true && data['rescheduled'] != 'true' && data['canceled'] == false  && datas['rescheduled'] != true && datas['rescheduled'] != 'true'){
-									
+									postDetailss.message= postDetails.name+' is Scheduled ';
 									datas['application_status'][datas['application_status'].length-1]['created']= new Date();
 									
 									if(datas['events'] && datas['events'].length && datas['events'].length !=0){
@@ -80,7 +93,7 @@ module.exports = async function create(request, response) {
 									
 								}else if(data['rescheduled'] == true || data['rescheduled'] == 'true' ){
 									datas['application_status'][datas['application_status'].length-1]['rescheduled']= new Date();
-									
+									postDetailss.message= postDetails.name+' is Canceled ';
 									
 									if(datas['events'] && datas['events'].length && datas['events'].length !=0){
 										var arrayVal = data['events'];
@@ -98,6 +111,8 @@ module.exports = async function create(request, response) {
 									}
 									
 								}else{
+									postDetailss.message= postDetails.name+' is Rescheduled ';
+									
 									datas['application_status'][datas['application_status'].length-1]['created']= new Date();
 									
 									if(datas['events'] && datas['events'].length && datas['events'].length !=0){
@@ -118,26 +133,34 @@ module.exports = async function create(request, response) {
 							
 							
 							var _response_objects = {'events': datas['events'],'invite_status': false,'application_status': datas['application_status'],'reschedule_url': data['reschedule_url'],'cancel_url': data['cancel_url'],'canceled': data['canceled'],'rescheduled': data['rescheduled']};
-							console.log('1');
-							console.log(_response_objects);
+							
 							JobApplications.update(datas.id,_response_objects).then(da=>{
 								return response.status(200).json(da);
 							});
+							postDetailss.name=titleName;
+							postDetailss.title=titleName;
+							postDetailss.account=datas['employer'];	
+							postDetailss.user_id=datas['user'];
+							postDetailss.job_id=datas['job_posting'];
+							postDetailss.employer=datas['employer'];		
+							postDetailss.view=0;	
 
+							Notification.create(postDetailss, function(err, job) {
+
+							});
 						}
 						
 					})
 				}
 			   
-			   }).catch(async function(err) {
-				   await errorBuilder.build(err, function(error_obj) {
-					   _response_object.errors = error_obj;
-					   _response_object.count = error_obj.length;
-					   return response.status(500).json(_response_object);
-				   });
-				   });
-		  }
-		  
-		});
+			}).catch(async function(err) {
+				await errorBuilder.build(err, function(error_obj) {
+					_response_object.errors = error_obj;
+					_response_object.count = error_obj.length;
+					return response.status(500).json(_response_object);
+				});
+			});
+		}
+	});
 		
 }
