14cacd575b5b2e90bb80015133b27650241c33a2
ArunKumar <arunkumar.s@agnitio-systems.com>
2021-12-17 16:15:53 +0530

    Bug Fixing

--- API/api/controllers/jobpostings/update.js
+++ API/api/controllers/jobpostings/update.js
@@ -177,7 +177,7 @@ module.exports = function create(request, response) {
 		LEFT JOIN job_location "locations" ON (locations.jobid= job_posting.id) 
 		WHERE (locations.status = 1 OR locations.status = 98 )  AND user_profile.job_type && ARRAY[job_posting.type]::TEXT[] AND (job_posting.id = ${parseInt(updated_job.id)}) AND
 		(user_account.status=1) AND (user_profile.work_authorization = 1 OR (( user_profile.country like locations.country OR  user_profile.other_countries && ARRAY[locations.country]::TEXT[] ) AND (( user_profile.city like locations.city OR  user_profile.other_cities && ARRAY[locations.city]::TEXT[] ) OR user_profile.willing_to_relocate =true ) ) )
-		AND (COALESCE(user_profile.experience) >= job_posting.experience) AND (COALESCE(user_profile.experience) <= 2 ) group by user_profile.id ,locations.id,locations.country`
+		AND (COALESCE(user_profile.experience) >= job_posting.experience) AND (COALESCE(user_profile.experience) <= 2 ) group by user_profile.id ,locations.country`
 					}else{
 						var Count_Users = `SELECT  locations.country as "job_country",locations.id as "location_id",user_profile.* as "job_id" FROM user_employments "job_posting"
 		CROSS JOIN user_profiles "user_profile" 
@@ -185,7 +185,7 @@ module.exports = function create(request, response) {
 		LEFT JOIN job_location "locations" ON (locations.jobid= job_posting.id) 
 		WHERE (locations.status = 1 OR locations.status = 98 )  AND user_profile.job_type && ARRAY[job_posting.type]::TEXT[] AND (job_posting.id = ${parseInt(updated_job.id)}) AND
 		(user_account.status=1) AND ( user_profile.country like locations.country OR  user_profile.other_countries && ARRAY[locations.country]::TEXT[] ) AND ( user_profile.city like locations.city OR  user_profile.other_cities && ARRAY[locations.city]::TEXT[] OR user_profile.willing_to_relocate =true ) 
-		AND (COALESCE(user_profile.experience) >= job_posting.experience)  AND (COALESCE(user_profile.experience) <= 2 ) group by user_profile.id ,locations.id,locations.country`
+		AND (COALESCE(user_profile.experience) >= job_posting.experience)  AND (COALESCE(user_profile.experience) <= 2 ) group by user_profile.id ,locations.country`
 					}
 
 				}else{
@@ -198,7 +198,7 @@ module.exports = function create(request, response) {
 		WHERE (locations.status = 1 OR locations.status = 98 )  AND user_profile.job_type && ARRAY[job_posting.type]::TEXT[] AND (job_posting.id = ${parseInt(updated_job.id)}) AND
 		(user_account.status=1) AND (user_profile.work_authorization = 1 OR (( user_profile.country like locations.country OR  user_profile.other_countries && ARRAY[locations.country]::TEXT[] ) AND ( ( user_profile.city like locations.city OR  user_profile.other_cities && ARRAY[locations.city]::TEXT[] ) OR user_profile.willing_to_relocate =true ) ) ) AND 
 		(( user_profile.hands_on_skills && job_posting.hands_on_skills ) OR ((COALESCE(job_posting.experience) <= 2 ) AND job_posting.entry = true))
-		AND (COALESCE(user_profile.experience) >= job_posting.experience) group by user_profile.id ,locations.id,locations.country`
+		AND (COALESCE(user_profile.experience) >= job_posting.experience) group by user_profile.id ,locations.country`
 					}else{
 						var Count_Users = `SELECT  locations.country as "job_country",locations.id as "location_id",user_profile.* as "job_id" FROM user_employments "job_posting"
 		CROSS JOIN user_profiles "user_profile" 
@@ -207,7 +207,7 @@ module.exports = function create(request, response) {
 		WHERE (locations.status = 1 OR locations.status = 98 )  AND user_profile.job_type && ARRAY[job_posting.type]::TEXT[] AND (job_posting.id = ${parseInt(updated_job.id)}) AND
 		(user_account.status=1) AND (( user_profile.country like locations.country OR  user_profile.other_countries && ARRAY[locations.country]::TEXT[] ) AND ( ( user_profile.city like locations.city OR  user_profile.other_cities && ARRAY[locations.city]::TEXT[] )OR user_profile.willing_to_relocate =true ) ) AND  
 		(( user_profile.hands_on_skills && job_posting.hands_on_skills ) OR ((COALESCE(job_posting.experience) <= 2 ) AND job_posting.entry = true))
-		AND (COALESCE(user_profile.experience) >= job_posting.experience) group by user_profile.id ,locations.id,locations.country`
+		AND (COALESCE(user_profile.experience) >= job_posting.experience) group by user_profile.id ,locations.country`
 					}
 				}
 				var del = await Scoring.destroy({ job_id: updated_job.id });
