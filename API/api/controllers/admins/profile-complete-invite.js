5907a57a0a718faaacdcfc8049fed14e1f3dd497
Vishal M <vishal.m@agnitio-systems.com>
2022-09-14 18:24:03 +0530

    add msg and sub

--- API/api/controllers/admins/profile-complete-invite.js
+++ API/api/controllers/admins/profile-complete-invite.js
@@ -10,11 +10,12 @@ module.exports = async function invite(request,response) {
     const user_id=request.body.id;
 	
 	UserProfiles.find({id:user_id}).then(data=>{
+		data[0].message=request.body.message;
 		const mail_data = {
 					template: 'users/profile-invite',
 					data: data[0],
 					to: data[0].email,
-					subject: 'Welcome to SAP WORLD.'
+					subject: request.body.subject
 				};
 	    //To send mail
 		mailService.sendMail(mail_data);
